b46521e2f257e1691703d4e0754591afe7075193
Mariana Bálsamo <44822014+MarianBal@users.noreply.github.com>
2019-08-04 12:26:43 -0300

    Add files via upload

---
+++ javascript/form.js
@@ -0,0 +1,8 @@
+console.log('hello world')
+
+//hamb
+document.querySelector('.fa-bars').onclick=()=>document.querySelector('.hamb').classList.toggle('hidden')
+
+
+//terms
+document.querySelector('#terms').onclick =()=>document.querySelector('.terms').classList.remove('hidden');
