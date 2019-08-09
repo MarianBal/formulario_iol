5ca458f04ad860609d2a422bcd0529cde03e1638
Mariana Bálsamo <44822014+MarianBal@users.noreply.github.com>
2019-08-09 15:31:42 -0300

    Add files via upload

--- javascript/form.js
+++ javascript/form.js
@@ -14,15 +14,17 @@ document.querySelectorAll('.close-modal').forEach(f=>f.onclick =()=> f.parentNod
 //form
 const form = document.querySelector('form');
 
-let send = true;
+let send = false;
 
 form.onsubmit= e =>{
   e.preventDefault();
 
+  let inputFlag = true;
+
   form.querySelectorAll('input').forEach(i=>{
     i.value.trim() ? i.parentNode.querySelector('.fa-exclamation-circle').classList.add('hidden') : i.parentNode.querySelector('.fa-exclamation-circle').classList.remove('hidden');
     i.value.trim() ? i.parentNode.querySelector('.font-body-3').classList.add('hidden') : i.parentNode.querySelector('.font-body-3').classList.remove('hidden');
-    i.value.trim() ? send = true : send = false;
+    i.value.trim() ? '': inputFlag = false;
 
   })
 
@@ -30,12 +32,11 @@ form.onsubmit= e =>{
   const emailValidate = /^\w.*@\w+\.\w/.test(email.value);
 
   emailValidate == false ? email.parentNode.querySelector('.font-body-3').classList.remove('hidden') : ''; 
-  emailValidate == true ? send = true : send = false;
+
 
   const validateEmail= document.getElementById('validate-mail');
 
-  validateEmail.value == email.value ? '' : validateEmail.parentNode.querySelector('.font-body-3').classList.remove('hidden'); 
-  email.value ? send = true : send = false;
+  validateEmail.value == email.value ? '' : document.getElementById('error').classList.remove('hidden');
   
   const password= document.getElementById('password');
 
@@ -43,26 +44,22 @@ form.onsubmit= e =>{
 
     document.getElementById('character').classList.add('error-circle');
     document.getElementById('character-text').classList.add('error-text');
-    send = false;
   
   }else{
 
     document.getElementById('character').classList.add('validate-circle');
     document.getElementById('character-text').classList.add('validate-text');
-    send = true;
-
   }
 
   const number = document.getElementById('number');
   const numberText = document.getElementById('number-text');
 
-  if (!password.value.match(/^[0-9]/)){
+  if (!password.value.match(/[0-9]/)){
     
     number.classList.add('error-circle')
     number.classList.remove('validate-circle');
     numberText.classList.add('error-text');
     numberText.classList.remove('validate-text');
-    send = false;
   
   }else if(password.value.match(/^[0-9]/)){
 
@@ -70,9 +67,10 @@ form.onsubmit= e =>{
     number.classList.remove('error-circle');
     numberText.classList.add('validate-text');
     numberText.classList.remove('error-text');
-    send = true;
-
   }
+
+  inputFlag == true && emailValidate && email.value && password.value.trim()>7 && password.value.match(/^[0-9]/) ? send = true : send = false;
+
   send==true &&form.submit();
 }
 
