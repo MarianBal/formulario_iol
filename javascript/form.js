b648e697245bc8826fe68c5c0da45ea5b36a0e5a
Mariana Bálsamo <44822014+MarianBal@users.noreply.github.com>
2019-08-09 11:56:30 -0300

    Add files via upload

--- javascript/form.js
+++ javascript/form.js
@@ -1,6 +1,3 @@
-console.log('hello world')
-
-
 
 //hamb
 document.querySelector('.fa-bars').onclick=()=>document.querySelector('.hamb').classList.toggle('hidden')
@@ -18,39 +15,65 @@ document.querySelectorAll('.close-modal').forEach(f=>f.onclick =()=> f.parentNod
 //form
 const form = document.querySelector('form');
 
+let send = true;
+
 form.onsubmit= e =>{
   e.preventDefault();
 
   form.querySelectorAll('input').forEach(i=>{
     i.value.trim() ? i.parentNode.querySelector('.fa-exclamation-circle').classList.add('hidden') : i.parentNode.querySelector('.fa-exclamation-circle').classList.remove('hidden');
     i.value.trim() ? i.parentNode.querySelector('.font-body-3').classList.add('hidden') : i.parentNode.querySelector('.font-body-3').classList.remove('hidden');
+    i.value.trim() ? send = true : send = false;
+
   })
 
   const email= document.getElementById('email');
   const emailValidate = /^\w.*@\w+\.\w/.test(email.value);
 
   emailValidate == false ? email.parentNode.querySelector('.font-body-3').classList.remove('hidden') : ''; 
+  emailValidate == true ? send = true : send = false;
 
   const validateEmail= document.getElementById('validate-mail');
 
   validateEmail.value == email.value ? '' : validateEmail.parentNode.querySelector('.font-body-3').classList.remove('hidden'); 
+  email.value ? send = true : send = false;
   
   const password= document.getElementById('password');
 
-  if(password.value.trim()<8){
+  if(password.value.trim()<=7){
 
     document.getElementById('character').classList.add('error-circle');
     document.getElementById('character-text').classList.add('error-text');
+    send = false;
   
   }else{
 
     document.getElementById('character').classList.add('validate-circle');
     document.getElementById('character-text').classList.add('validate-text');
+    send = true;
 
   }
-  //password.parentNode.parentNode.querySelector('.character').classList.add('.error-text'):password.parentNode.parentNode.querySelector('.character').classList.add('.validate-text');
 
+  const number = document.getElementById('number');
+  const numberText = document.getElementById('number-text');
 
+  if (!password.value.match(/^[0-9]/)){
+    
+    number.classList.add('error-circle')
+    number.classList.remove('validate-circle');
+    numberText.classList.add('error-text');
+    numberText.classList.remove('validate-text');
+    send = false;
+  
+  }else if(password.value.match(/^[0-9]/)){
+
+    number.classList.add('validate-circle');
+    number.classList.remove('error-circle');
+    numberText.classList.add('validate-text');
+    numberText.classList.remove('error-text');
+    send = true;
+
+  }
 }
 
 
